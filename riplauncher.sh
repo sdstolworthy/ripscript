5d3d60f659da4b23fc27b1d8b94ca1692c56043f
Spencer Stolworthy <sstolworthy@altamir.io>
2018-12-26 20:00:12 -0700

    more edits

--- riplauncher.sh
+++ riplauncher.sh
@@ -7,4 +7,4 @@ touch /var/log/ripscript.log
     echo "<<<<<<<<<<DRIVE IS: $1>>>>>>>>>>>>"
     echo -e "ARG1=$1" > /etc/.dvdripconf
     systemctl start dvdrip@$SERVNUM.service
-} &>> /var/log/ripscript.log
+} &>> /var/log/rip.log
