90d8d1af05b05e2359b820c0d6d89abff60e3f4e
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-01-03 13:54:21 -0500

    fixes

--- ripscript.sh
+++ ripscript.sh
@@ -8,11 +8,18 @@ fi
     date
     mount -a
     if [[ $1 == *"/dev/sr"* ]]; then
-        DISNUM=$(echo $1 | grep -oP '\d')
+        IFS='
+'
+	for line in $(makemkvcon -r --cache=1 info disk:9999); do
+        	if [[ $line == *"$(echo $1)"* ]]; then
+                	echo $line
+                	DISNUM=`echo "$line" | cut -d ',' -f 1 | cut -d ':' -f 2`
+                	echo $DISNUM
+        	fi
+	done
     else
         echo "Error finding correct disk number. Found $DISNUM, but this number does not seem to exist"
     fi
-    
     DVDNAME=$(makemkvcon -r info)
     DVDNAME=`echo "$DVDNAME" | grep "DRV:$DISNUM\+"`
     DVDNAME=${DVDNAME:53}
@@ -20,7 +27,7 @@ fi
     DVDNAME=${DVDNAME:0:$len}
     
     
-    echo "drive is: $1"
+    echo "\ndrive is: $1\n\n"
     DVDNAME=$(isoinfo -i $1 -d | grep "Volume id:" | awk '{print $3}')
     
     if [[ ${DVDNAME^^} == DVD_VIDEO ]]; then
@@ -31,7 +38,7 @@ fi
     echo ">>>>>>>>>>>>>>>>>>>>>>>>Beginning script to rip: $DVDNAME"
     echo $USER
     
-    echo "THIS IS YOUR DISC NUMBER: $DISNUM"
+    echo "\nTHIS IS YOUR DISC NUMBER: $DISNUM\n\n"
     mkdir -p $WORKPATH
     rm $WORKPATH/*
     makemkvcon --minlength=3600 -r --decrypt --directio=true mkv disc:$DISNUM all $WORKPATH
