3ea8f66a670c6e01777203df7aa7e9c5799e93f6
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-12-26 10:04:58 -0600

    updated install

--- install.sh
+++ install.sh
@@ -16,13 +16,13 @@ mkdir -p /var/rips
 
 echo "Installing files"
 chmod a+x ./dvdrip@.service
-cp ./dvdrip@.service /etc/systemd/system
+/bin/cp -rf ./dvdrip@.service /etc/systemd/system
 
 chmod a+x ./ripscript.sh
-cp ./ripscript.sh /usr/bin/ripscript
+/bin/cp -rf ./ripscript.sh /usr/bin/ripscript
 
 chmod a+x ./riplauncher.sh
-cp ./riplauncher.sh /usr/bin/riplauncher
+/bin/cp -rf ./riplauncher.sh /usr/bin/riplauncher
 
 echo "Adding rules to udev"
 cat udev.rules > /etc/udev/rules.d/99-sr.rules
